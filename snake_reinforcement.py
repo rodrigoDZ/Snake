9ff2ef43b75b0425624a550f8a879c95edf060f2
Rodrigo <rodrigodiaz018@gmail.com>
2020-08-26 19:29:29 -0500

    Creacion de  la ventana inicial del juego

--- snake_reinforcement.py
+++ snake_reinforcement.py
@@ -1,2 +1,16 @@
 import pygame
+from pygame.locals import *
+import sys
 
+#Pantalla inicial
+pygame.init()
+pantalla = pygame.display.set_mode((800,800))
+pygame.display.set_caption("Snake")
+
+
+
+while True:
+    
+    for eventos in pygame.event.get():
+        if eventos.type == pygame.QUIT:
+            sys.exit()
